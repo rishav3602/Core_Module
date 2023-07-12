5c93393a78d391a2b693f319cbd786a6e405e5a6
rishav3602 <rishavkumar7294@gmail.com>
2023-07-12 19:36:00 +0530

    first commit

---
+++ assignment3.py
@@ -0,0 +1,311 @@
+"""
+1. Scenario: A company wants to analyze the sales performance of its products in different regions. They have collected the following data:
+   Region A: [10, 15, 12, 8, 14]
+   Region B: [18, 20, 16, 22, 25]
+   Calculate the mean sales for each region.
+
+
+Ans :-
+Region A: [10, 15, 12, 8, 14]
+Region B: [18, 20, 16, 22, 25]
+Mean sales for Region A: 11.8
+Mean sales for Region B: 20.2
+
+
+"""
+
+"""
+2. Scenario: A survey is conducted to measure customer satisfaction on a scale of 1 to 5. The data collected is as follows:
+   [4, 5, 2, 3, 5, 4, 3, 2, 4, 5]
+   Calculate the mode of the survey responses.
+
+
+Ans :-
+ Survey responses: [4, 5, 2, 3, 5, 4, 3, 2, 4, 5]
+Mode: 4
+
+   
+"""
+
+
+"""
+3. Scenario: A company wants to compare the salaries of two departments. The salary data for Department A and Department B are as follows:
+   Department A: [5000, 6000, 5500, 7000]
+   Department B: [4500, 5500, 5800, 6000, 5200]
+   Calculate the median salary for each department.
+
+ans :- Department A: [5000, 6000, 5500, 7000]
+Department B: [4500, 5500, 5800, 6000, 5200]
+Median salary for Department A: 5750.0
+Median salary for Department B: 5500.0
+
+
+   """
+
+
+"""
+4. Scenario: A data analyst wants to determine the variability in the daily stock prices of a company. The data collected is as follows:
+   [25.5, 24.8, 26.1, 25.3, 24.9]
+   Calculate the range of the stock prices.
+
+ans :- Stock prices: [25.5, 24.8, 26.1, 25.3, 24.9]
+Range: 1.3
+
+   """
+
+
+
+"""5. Scenario: A study is conducted to compare the performance of two different teaching methods. The test scores of the students in each group are as follows:
+   Group A: [85, 90, 92, 88, 91]
+   Group B: [82, 88, 90, 86, 87]
+   Perform a t-test to determine if there is a significant difference in the mean scores between the two groups.
+
+ans :- 
+Perform a t-test to determine if there is a significant difference in the mean scores between the two groups. (Assuming a two-sample unpaired t-test with equal variances)
+
+Group A: [85, 90, 92, 88, 91]
+Group B: [82, 88, 90, 86, 87]
+t-statistic: -0.366, p-value: 0.723 (assuming a significance level of 0.05, there is no significant difference)
+
+
+
+   """
+
+
+"""
+6. Scenario: A company wants to analyze the relationship between advertising expenditure and sales. The data collected is as follows:
+   Advertising Expenditure (in thousands): [10, 15, 12, 8, 14]
+   Sales (in thousands): [25, 30, 28, 20, 26]
+   Calculate the correlation coefficient between advertising expenditure and sales.
+
+   ans :- 
+
+   Advertising Expenditure (in thousands): [10, 15, 12, 8, 14]
+Sales (in thousands): [25, 30, 28, 20, 26]
+Correlation coefficient: 0.928 (indicating a strong positive correlation)
+
+
+   """
+
+
+"""
+7. Scenario: A survey is conducted to measure the heights of a group of people. The data collected is as follows:
+   [160, 170, 165, 155, 175, 180, 170]
+   Calculate the standard deviation of the heights.
+
+ans :- 
+Heights: [160, 170, 165, 155, 175, 180, 170]
+Standard deviation: 8.391
+
+   """
+
+
+"""
+8. Scenario: A company wants to analyze the relationship between employee tenure and job satisfaction. The data collected is as follows:
+   Employee Tenure (in years): [2, 3, 5, 4, 6, 2, 4]
+   Job Satisfaction (on a scale of 1 to 10): [7, 8, 6, 9, 5, 7, 6]
+   Perform a linear regression analysis to predict job satisfaction based on employee tenure.
+
+ans :-
+ Perform a linear regression analysis to predict job satisfaction based on employee tenure.
+
+
+
+   """
+
+
+"""
+9. Scenario: A study is conducted to compare the effectiveness of two different medications. The recovery times of the patients in each group are as follows:
+   Medication A: [10, 12, 14, 11, 13]
+   Medication B: [15, 17, 16, 14, 18]
+   Perform an analysis of variance (ANOVA) to determine if there is a significant difference in the mean recovery times between the two medications.
+
+ans :- 
+ Perform an analysis of variance (ANOVA) to determine if there is a significant difference in the mean recovery times between the two medications.
+
+
+
+   """
+
+
+"""
+10. Scenario: A company wants to analyze customer feedback ratings on a scale of 1 to 10. The data collected is
+
+ as follows:
+    [8, 9, 7, 6, 8, 10, 9, 8, 7, 8]
+    Calculate the 75th percentile of the feedback ratings.
+ 
+
+ans :- Feedback ratings: [8, 9, 7, 6, 8, 10, 9, 8, 7, 8]
+75th percentile: 8.75
+
+
+
+    """
+
+
+"""
+11. Scenario: A quality control department wants to test the weight consistency of a product. The weights of a sample of products are as follows:
+    [10.2, 9.8, 10.0, 10.5, 10.3, 10.1]
+    Perform a hypothesis test to determine if the mean weight differs significantly from 10 grams.
+
+
+ans:- hypothesis test perform
+
+
+    """
+
+
+"""
+12. Scenario: A company wants to analyze the click-through rates of two different website designs. The number of clicks for each design is as follows:
+    Design A: [100, 120, 110, 90, 95]
+    Design B: [80, 85, 90, 95, 100]
+    Perform a chi-square test to determine if there is a significant difference in the click-through rates between the two designs.
+
+ans :- chi_square test
+
+
+"""
+
+
+"""
+13. Scenario: A survey is conducted to measure customer satisfaction with a product on a scale of 1 to 10. The data collected is as follows:
+    [7, 9, 6, 8, 10, 7, 8, 9, 7, 8]
+    Calculate the 95% confidence interval for the population mean satisfaction score.
+
+ans :- Satisfaction scores: [7, 9, 6, 8, 10, 7, 8, 9, 7, 8]
+Confidence interval: (7.175, 8.825)
+
+    """
+
+
+"""
+14. Scenario: A company wants to analyze the effect of temperature on product performance. The data collected is as follows:
+    Temperature (in degrees Celsius): [20, 22, 23, 19, 21]
+    Performance (on a scale of 1 to 10): [8, 7, 9, 6, 8]
+    Perform a simple linear regression to predict performance based on temperature.
+
+ans :- linear regression
+
+
+    """
+
+
+"""
+15. Scenario: A study is conducted to compare the preferences of two groups of participants. The preferences are measured on a Likert scale from 1 to 5. The data collected is as follows:
+    Group A: [4, 3, 5, 2, 4]
+    Group B: [3, 2, 4, 3, 3]
+    Perform a Mann-Whitney U test to determine if there is a significant difference in the median preferences between the two groups.
+    """
+
+
+"""
+16. Scenario: A company wants to analyze the distribution of customer ages. The data collected is as follows:
+    [25, 30, 35, 40, 45, 50, 55, 60, 65, 70]
+    Calculate the interquartile range (IQR) of the ages.
+
+ans :- Ages: [25, 30, 35, 40, 45, 50, 55, 60, 65, 70]
+IQR: 30
+
+    """
+
+
+"""
+17. Scenario: A study is conducted to compare the performance of three different machine learning algorithms. The accuracy scores for each algorithm are as follows:
+    Algorithm A: [0.85, 0.80, 0.82, 0.87, 0.83]
+    Algorithm B: [0.78, 0.82, 0.84, 0.80, 0.79]
+    Algorithm C: [0.90, 0.88, 0.89, 0.86, 0.87]
+    Perform a Kruskal-Wallis test to determine if there is a significant difference in the median accuracy scores between the algorithms.
+    """
+
+
+"""
+18. Scenario: A company wants to analyze the effect of price on sales. The data collected is as follows:
+    Price (in dollars): [10, 15, 12, 8, 14]
+    Sales: [100, 80, 90, 110, 95]
+    Perform a simple linear regression to predict
+
+ sales based on price.
+
+
+ ans - simple linear regresssion
+
+ """
+
+
+"""
+19. Scenario: A survey is conducted to measure the satisfaction levels of customers with a new product. The data collected is as follows:
+    [7, 8, 9, 6, 8, 7, 9, 7, 8, 7]
+    Calculate the standard error of the mean satisfaction score.
+
+ans :- 
+sample mean :-
+Satisfaction scores: [7, 8, 9, 6, 8, 7, 9, 7, 8, 7]
+Total scores: 10
+
+Sample mean = (7 + 8 + 9 + 6 + 8 + 7 + 9 + 7 + 8 + 7) / 10 = 77 / 10 = 7.7
+
+
+standard deviation = 
+Sample mean = 7.7
+Number of scores (n) = 10
+
+Step 1: Subtract the sample mean from each score and square the differences:
+(7 - 7.7)^2 = 0.49
+(8 - 7.7)^2 = 0.09
+(9 - 7.7)^2 = 1.69
+(6 - 7.7)^2 = 2.89
+(8 - 7.7)^2 = 0.09
+(7 - 7.7)^2 = 0.49
+(9 - 7.7)^2 = 1.69
+(7 - 7.7)^2 = 0.49
+(8 - 7.7)^2 = 0.09
+(7 - 7.7)^2 = 0.49
+
+Step 2: Sum up the squared differences:
+0.49 + 0.09 + 1.69 + 2.89 + 0.09 + 0.49 + 1.69 + 0.49 + 0.09 + 0.49 = 7.41
+
+Step 3: Divide the sum by (n-1):
+7.41 / (10-1) = 7.41 / 9 = 0.8233 (rounded to four decimal places)
+
+Step 4: Take the square root:
+sqrt(0.8233) = 0.907 (rounded to three decimal places)
+
+
+standard error of the mean :- 
+Sample standard deviation = 0.907
+Sample size (n) = 10
+
+Standard error of the mean = 0.907 / sqrt(10) = 0.287 (rounded to three decimal places)
+
+
+    """
+
+
+"""
+20. Scenario: A company wants to analyze the relationship between advertising expenditure and sales. The data collected is as follows:
+    Advertising Expenditure (in thousands): [10, 15, 12, 8, 14]
+    Sales (in thousands): [25, 30, 28, 20, 26]
+    Perform a multiple regression analysis to predict sales based on advertising expenditure.
+
+    import statsmodels.api as sm
+
+# Organize the data
+advertising_expenditure = [10, 15, 12, 8, 14]
+sales = [25, 30, 28, 20, 26]
+
+# Add a constant term to the independent variable
+advertising_expenditure = sm.add_constant(advertising_expenditure)
+
+# Create the regression model
+model = sm.OLS(sales, advertising_expenditure)
+
+# Fit the model to the data
+results = model.fit()
+
+# Print the summary of the regression analysis
+print(results.summary())
+
+    """
+
+
